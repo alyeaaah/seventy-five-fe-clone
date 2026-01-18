e2cc45eee4d912d2330336b6f306ba41264db297
zzaula <mochamadzaula@gmail.com>
2026-01-18 16:44:12 +0700

    Revert "revert here for hybrid developp"
    This reverts commit ae20bfa9e9e2501f24637a278c02a072778340ef.

--- pages/[...paths].tsx
+++ pages/[...paths].tsx
@@ -1,34 +1,5 @@
-import { useEffect } from "react";
+// import dynamic from "next/dynamic";
 
-export default function CatchAll() {
-  useEffect(() => {
-    // Load the Vite app dynamically
-    const script = document.createElement('script');
-    script.type = 'module';
-    script.src = '/assets/index-CDIgYamD.js';
-    document.body.appendChild(script);
-
-    // Load the CSS
-    const link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = '/assets/index-Dgihpmma.css';
-    document.head.appendChild(link);
-
-    return () => {
-      // Cleanup if needed
-      if (document.body.contains(script)) {
-        document.body.removeChild(script);
-      }
-      if (document.head.contains(link)) {
-        document.head.removeChild(link);
-      }
-    };
-  }, []);
-
-  return (
-    <div>
-      <div id="root"></div>
-      <div>Loading application...</div>
-    </div>
-  );
-}
+// export default dynamic(() => import("../src"), {
+//   ssr: false,
+// });
